c340f091fc1fef49ddeae05d945743f0790be782
webbrowser11 <terpstragraham@gmail.com>
2024-06-22 15:07:47 -0700

    Update kernel.py

--- kernel.py
+++ kernel.py
@@ -1,3 +1,5 @@
+import subprocess
+import os
 print("welcome to the SCA kernel lots of contributors worked on this!")
 print("hope you find this program useful!")
 while True:
@@ -6,6 +8,7 @@ while True:
     print("help")
     print("info")
     print("echo")
+    print("helloworld.app")
   elif command == "info":
     print("developed by the SCA all right reserved.")
     print("this kernel may not be reproduced in any way.")
@@ -13,5 +16,10 @@ while True:
   elif command == "echo":
     echotxt = input("echo what:")
     print(echotxt)
+  elif command == "helloworld.app":
+    scripts_to_run = ['helloworldapp.py']
+    for s in scripts_to_run:
+      subprocess.Popen([os.path.join(os.getcwd(), s)])
+    break
   else:
     print("not a command type help for a list of commands")
