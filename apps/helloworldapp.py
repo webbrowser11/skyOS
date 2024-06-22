2996cb1640808a9720d649c7c356f7a51cdeae25
webbrowser11 <terpstragraham@gmail.com>
2024-06-22 15:52:44 -0700

    Update helloworldapp.py

--- apps/helloworldapp.py
+++ apps/helloworldapp.py
@@ -1,4 +1,13 @@
-print("hello world!")
-while True:
-  elif:
-    
+import subprocess
+import os
+import sys
+
+def main():
+    print("Hello, World from hello world app!")
+    # Path to the kernel script
+    kernel_script = os.path.join(os.getcwd(), 'kernel.py')
+    # Run the kernel script as a subprocess
+    subprocess.run([sys.executable, kernel_script])
+
+if __name__ == "__main__":
+    main()
