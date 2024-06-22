375389fb7cf66a5b654ee2f854c068edddaab79f
webbrowser11 <terpstragraham@gmail.com>
2024-06-22 14:51:59 -0700

    Create kernel.py

---
+++ kernel.py
@@ -0,0 +1,18 @@
+print("welcome to the SCA kernel lots of contributors worked on this!")
+print("hope you find this program useful!")
+while True:
+  command = input("command:")
+  if command == "help":
+    print("help")
+    print("info")
+    print("echo")
+    echo
+  elif command == "info":
+    print("developed by the SCA all right reserved.")
+    print("this kernel may not be reproduced in any way.")
+    print("you can archive. (make sure the archive is public)")
+  elif command == "echo":
+    echotxt = input("echo what:")
+    print(echotxt)
+  else:
+    print("not a command type help for a list of commands")
