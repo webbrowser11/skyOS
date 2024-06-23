3e8fd6248742eddbbda36bff8f2a5543348947c2
Scratch_Fakemon <150092671+ScratchFakemon@users.noreply.github.com>
2024-06-23 13:26:11 -0500

    Create simpletext.py

---
+++ apps/simpletext.py
@@ -0,0 +1,15 @@
+import subprocess
+import os
+import sys
+
+def main():
+    print("Simple Text")
+    print("(What did you expect from an app called simpletext.py?)")
+    print("Oh, and by the way... 10 + 20 = " + 10+20 +"! How cool!")
+    # Path to the kernel script in the KERNEL folder
+    kernel_script = os.path.join(os.getcwd(), 'KERNEL', 'kernel.py')
+    # Run the kernel script as a subprocess
+    subprocess.run([sys.executable, kernel_script])
+
+if __name__ == "__main__":
+    main()
