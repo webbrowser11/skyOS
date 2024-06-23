134c4ab8aaa1ec28ed3e0fde348b135d461fc5f0
webbrowser11 <terpstragraham@gmail.com>
2024-06-22 19:38:46 -0700

    Update helloworldapp.py

--- apps/helloworldapp.py
+++ apps/helloworldapp.py
@@ -3,9 +3,10 @@ import os
 import sys
 
 def main():
-    print("Hello, World from hello world app!")
-    # Path to the kernel script
-    kernel_script = os.path.join(os.getcwd(), 'kernel.py')
+    print("Hello, World!")
+    print("make an app in the app folder (with the same structure just modified to fit your app code.)")
+    # Path to the kernel script in the KERNEL folder
+    kernel_script = os.path.join(os.getcwd(), 'KERNEL', 'kernel.py')
     # Run the kernel script as a subprocess
     subprocess.run([sys.executable, kernel_script])
 
